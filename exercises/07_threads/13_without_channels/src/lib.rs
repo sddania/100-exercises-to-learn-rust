e9404f2613d6a8a8d7720adb21a574145f936363
Daniele Agosti <sddania@gmail.com>
2025-07-06 14:41:34 +0200

    part 07 resolved

--- exercises/07_threads/13_without_channels/src/lib.rs
+++ exercises/07_threads/13_without_channels/src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: You don't actually have to change anything in the library itself!
+//  You don't actually have to change anything in the library itself!
 //  We mostly had to **remove** code (the client type, the launch function, the command enum)
 //  that's no longer necessary.
 //  Fix the `todo!()` in the testing code and see how the new design can be used.
